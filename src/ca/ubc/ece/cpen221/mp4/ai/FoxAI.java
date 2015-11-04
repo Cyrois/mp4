f316a88c6b28d25a9f0d74de845e019e627d22cd
Cyrois <cyrois3@gmail.com>
2015-11-04 13:50:35 -0800

    foxai is working

--- src/ca/ubc/ece/cpen221/mp4/ai/FoxAI.java
+++ src/ca/ubc/ece/cpen221/mp4/ai/FoxAI.java
@@ -7,8 +7,12 @@ import java.util.Set;
 import ca.ubc.ece.cpen221.mp4.ArenaWorld;
 import ca.ubc.ece.cpen221.mp4.Direction;
 import ca.ubc.ece.cpen221.mp4.Location;
+import ca.ubc.ece.cpen221.mp4.Util;
+import ca.ubc.ece.cpen221.mp4.World;
+import ca.ubc.ece.cpen221.mp4.commands.BreedCommand;
 import ca.ubc.ece.cpen221.mp4.commands.Command;
 import ca.ubc.ece.cpen221.mp4.commands.EatCommand;
+import ca.ubc.ece.cpen221.mp4.commands.InvalidCommandException;
 import ca.ubc.ece.cpen221.mp4.commands.MoveCommand;
 import ca.ubc.ece.cpen221.mp4.commands.WaitCommand;
 import ca.ubc.ece.cpen221.mp4.items.Item;
@@ -26,21 +30,42 @@ public class FoxAI extends AbstractAI {
 
 	@Override
 	public Command getNextAction(ArenaWorld world, ArenaAnimal animal) {
-		if(isFoodNearby(world, animal)){
+		//TODO implement breeding
+		if(shouldFoxBreed(animal)) {
+			return new BreedCommand(animal, Util.getRandomEmptyAdjacentLocation((World) world, animal.getLocation()));
+		} else if(isFoodNearby(world, animal)){
 			Item food = getClosestFood(world, animal);
-			if(1 == food.getLocation().getDistance(animal.getLocation())) {
+			if(isFoodEdible(animal, food)) {
 				return new EatCommand(animal, food);
 			} else {
-				Location newLocation = new Location(animal.getLocation(), getBestDirection(animal, food));
-				return new MoveCommand(animal, newLocation);
+				return getBestMove(world, animal, food);
 			}
+		} else {
+			return new WaitCommand();
 		}
-		return new WaitCommand();
+	}
+	
+	private boolean shouldFoxBreed(ArenaAnimal animal) {
+		int energy = animal.getEnergy();
+		int maxEnergy = animal.getMaxEnergy();
+		int breedEnergy = animal.getMinimumBreedingEnergy();
+
+		//TODO optimize this
+		int shouldBreedEnergy = maxEnergy / breedEnergy;
+		shouldBreedEnergy = maxEnergy / 2;
+		if(energy > shouldBreedEnergy) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
+	//check if there is food within sight
 	private boolean isFoodNearby(ArenaWorld world, ArenaAnimal animal) {
 		Set<Item> nearbyItems = world.searchSurroundings(animal);
-
+		
+		//assuming foxes will eat rabbits
+		//TODO add more food cases
 		for(Item item : nearbyItems) {
 			if(item.getName().equalsIgnoreCase("Rabbit")) {
 				return true;
@@ -49,8 +74,17 @@ public class FoxAI extends AbstractAI {
 
 		return false;
 	}
+	
+	//check if food is a distance of one block, thus edible
+	private boolean isFoodEdible(ArenaAnimal animal, Item food) {
+		if(1 == food.getLocation().getDistance(animal.getLocation())) {
+			return true;
+		} 
+		return false;
+		
+	}
 
-	//change to is food edible
+	//find the closest food item
 	private Item getClosestFood(ArenaWorld world, ArenaAnimal animal) {
 		Set<Item> nearbyItems = world.searchSurroundings(animal);
 		int closestFoodDistance = animal.getViewRange();
@@ -59,7 +93,7 @@ public class FoxAI extends AbstractAI {
 		for(Item item : nearbyItems) {
 			if(item.getName().equalsIgnoreCase("Rabbit")) {
 				int foodDistance = item.getLocation().getDistance(animal.getLocation());
-				if(foodDistance < closestFoodDistance) {
+				if(foodDistance <= closestFoodDistance) {
 					closestFoodDistance = foodDistance;
 					closestFood = item;
 				}
@@ -69,9 +103,16 @@ public class FoxAI extends AbstractAI {
 		return closestFood;
 	}
 	
-	//change to get best move
-	private Direction getBestDirection(ArenaAnimal animal, Item food) {
-		return animal.getLocation().getDirection(food.getLocation());
+	//move towards the food
+	private MoveCommand getBestMove(ArenaWorld world, ArenaAnimal animal, Item food) {
+		Direction moveDirection = Util.getDirectionTowards(animal.getLocation(), food.getLocation());
+		Location newLocation = new Location(animal.getLocation(), moveDirection);
+		if(Util.isLocationEmpty((World) world, newLocation)) {
+			return new MoveCommand(animal, newLocation);
+		} else {
+			newLocation = Util.getRandomEmptyAdjacentLocation((World) world, animal.getLocation());
+			return new MoveCommand(animal, newLocation);
+		}
 	}
 
 }
